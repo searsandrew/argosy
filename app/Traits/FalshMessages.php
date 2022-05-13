323ee5629a6aecafbe37f6b4de2383b6192a7050
Andrew Sears <searsandrew@gmail.com>
2022-05-13 15:39:45 -0500

    Create Flash Controller Trait

---
+++ app/Traits/FalshMessages.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Traits;
+
+trait FlashMessages
+{
+    protected $errorMessages = [];
+
+    protected $infoMessages = [];
+
+    protected $successMessages = [];
+
+    protected $warningMessages = [];
+
+    protected function setFlashMessage($message, $type)
+    {
+        switch ($type)
+        {
+            case 'error':
+                $model = 'errorMessages';
+                break;
+            
+            case 'success':
+                $model = 'successMessages';
+                break;
+
+            case 'warning':
+                $model = 'warningMessages';
+                break;
+
+            default:
+                $model = 'infoMessages';
+                break;
+        }
+
+        if (is_array($message)) {
+            foreach($message as $key => $value)
+            {
+                array_push($this->$model, $value);
+            }
+        } else {
+            array_push($this->$model, $message);
+        }
+    }
+
+    protected function getFlashMessage()
+    {
+        return [
+            'error' => $this->errorMessages,
+            'info' => $this->infoMessages,
+            'success' => $this->successMessages,
+            'warning' => $this->warningMessages,
+        ];
+    }
+
+    protected function showFlashMessages()
+    {
+        session()->flash('error', $this->errorMessages);
+        session()->flash('info', $this->infoMessages);
+        session()->flash('success', $this->successMessages);
+        session()->flash('warning', $this->warningMessages);
+    }
+}
